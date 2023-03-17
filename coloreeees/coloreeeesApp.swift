3fc5dfdd73dfb40b1350caf6155856f883f58923
Valentina <balendi.art@gmail.com>
2023-03-17 15:53:08 +0100

    Initial Commit

---
+++ coloreeees/coloreeeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  coloreeeesApp.swift
+//  coloreeees
+//
+//  Created by Apprenant 18 on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct coloreeeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
